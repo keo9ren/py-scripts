1c03890d178fd6b23a784f16d793cb58a945d115
keo9ren <okehret@gmail.com>
2016-11-24 11:09:55 +0000

    webmode enabled

---
+++ emacs/webmodesetup.el
@@ -0,0 +1,24 @@
+;;; package --- Summary
+;;; commentary:
+;;; Code:
+(require 'use-package)
+
+(use-package web-mode
+  :ensure t
+  :mode ("\\.phtml\\'"
+         "\\.html?\\'"
+         "\\.tpl\\.php\\'"
+         "\\.[agj]sp\\'"
+         "\\.as[cp]x\\'"
+         "\\.djhtml\\'"
+         "\\.erb\\'"
+         "\\.mustache\\'"
+         "\\.ts\\'"
+         "\\.tsx\\'"
+         "\\.js\\'"
+         "\\.jsx\\'"
+         "\\.json\\'"
+         )
+)
+(provide 'webmodesetup.el)
+;;; webmodesetup.el ends here
