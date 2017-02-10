e2cbb8c403c3db311cde4e8352cb9217167eed6f
Oliver Kehret <okehret@gmail.com>
2017-02-11 00:44:00 +0100

    formated-file

--- emacs/setup/rtagssetup.el
+++ emacs/setup/rtagssetup.el
@@ -5,14 +5,13 @@
 (load-file "/usr/share/emacs/site-lisp/rtags/company-rtags.el")
 (load-file "/usr/share/emacs/site-lisp/rtags/flycheck-rtags.el")
 (require 'use-package)
-(use-package rtags
-  :commands rtags
-  :bind(("C-c r s" . rtags-display-summary)
-        ("C-c r f" . rtags-find-symbol)
-        ("C-c r n" . rtags-rename-symbol)
-        ("C-c r r" . rtags-find-references)
-        ("C-c r v" . rtags-find-virtuals-at-point)
-       )
-  )
+(use-package 
+  rtags 
+  :commands rtags 
+  :bind(("C-c r s" . rtags-display-summary) 
+        ("C-c r f" . rtags-find-symbol) 
+        ("C-c r n" . rtags-rename-symbol) 
+        ("C-c r r" . rtags-find-references) 
+        ("C-c r v" . rtags-find-virtuals-at-point)))
 (provide 'rtagssetup.el)
 ;;; rtagssetup.el ends here
