b6a9241a253ec52725066d34d67639adac6782ea
Oliver Kehret <okehret@gmail.com>
2017-02-11 00:30:25 +0100

    smartparens for elisp

--- emacs/setup/smartparenssetup.el
+++ emacs/setup/smartparenssetup.el
@@ -5,7 +5,8 @@
 
 (use-package smartparens
   :ensure t
-  :init (add-hook 'js-mode-hook #'smartparens-mode)
+  :init (progn (add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
+         (add-hook 'js-mode-hook #'smartparens-mode))
 )
 (provide 'smartparenssetup.el)
 ;;; smartparenssetup.el ends here
