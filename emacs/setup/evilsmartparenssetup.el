86b518c086183a3422ef1021b8116714afe5c888
Oliver Kehret <okehret@gmail.com>
2017-02-11 00:34:31 +0100

    formated file

--- emacs/setup/evilsmartparenssetup.el
+++ emacs/setup/evilsmartparenssetup.el
@@ -3,9 +3,9 @@
 ;;; Code:
 (require 'use-package)
 
-(use-package evil-smartparens
-  :ensure t
-  :init (add-hook 'smartparens-enabled-hook #'evil-smartparens-mode)
-)
+(use-package 
+  evil-smartparens 
+  :ensure t 
+  :init (add-hook 'smartparens-enabled-hook #'evil-smartparens-mode))
 (provide 'evilsmartparenssetup.el)
 ;;; evilsmartparenssetup.el ends here
