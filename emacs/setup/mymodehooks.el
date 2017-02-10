f9b9905f4738022bd77827f65cac28f80cbaee59
Oliver Kehret <okehret@gmail.com>
2017-02-11 00:23:34 +0100

    elisp-formatted file

--- emacs/setup/mymodehooks.el
+++ emacs/setup/mymodehooks.el
@@ -2,152 +2,118 @@
 ;;; commentary:
 ;;; Code:
 
-(defun my-c++-mode-hooks ()
-  "MY-C++-MODE-HOOKS."
-  (interactive)
-  (cmake-ide-setup)
-  (setq rtags-completions-enabled t)
-  (setq semantic-mode 1)
-  (setq-local company-backends '(
-  (company-keywords
-  company-rtags
-  )
-  ))
-  (fci-mode)
-  (flyspell-prog-mode)
-)
-;
-(defun my-c-mode-hooks ()
-  "MY-C-MODE-HOOKS."
-  (interactive)
-  (cmake-ide-setup)
-  (setq semantic-mode 1)
-  (setq rtags-completions-enabled t)
-  (setq-local company-backends '(
-  (company-keywords
-  company-rtags
-  )
-  ))
-  (fci-mode)
-  (flyspell-prog-mode)
-)
-;
-(defun my-cmake-mode-hooks()
- "MY-CMAKE-MODE-HOOKS."
- (setq-local company-backends '((company-files company-cmake company-dabbrev-code)))
- (flyspell-prog-mode)
-)
-;
-(defun my-org-mode-hooks()
- "MY-ORG-MODE-HOOKS."
- (setq-local company-backends '((company-files company-dabbrev)))
- (flyspell-mode 1)
-)
-;
-(defun my-inferior-octave-mode-hooks()
- "MY-Inferior-OCTAVE-MODE-HOOKS."
- (turn-on-font-lock)
- (define-key inferior-octave-mode-map [up]
-        'comint-previous-input)
- (define-key inferior-octave-mode-map [down]
-  'comint-next-input)
-  )
-(defun my-octave-mode-hooks()
- "MY-OCTAVE-MODE-HOOKS."
- (setq-local company-backends '((company-files company-dabbrev-code)))
- (fci-mode)
- (set-fill-column 80)
- (auto-fill-mode 1)
- (abbrev-mode 1)
- (if (eq window-system 'x)
-    (font-lock-mode 1))
- )
- (flyspell-prog-mode)
-;
-(defun my-python-mode-hooks()
- "MY-PYTHON-MODE-HOOKS."
- (setq-local company-backends '((company-files company-jedi company-dabbrev-code)))
- (require 'sphinx-doc)
- (sphinx-doc-mode t)
- (require 'py-isort)
- (add-hook 'before-save-hook 'py-isort-before-save)
- (fci-mode)
- (set-fill-column 100)
- (flyspell-prog-mode)
-)
-;
-(defun my-nxml-mode-hooks()
- "MY-NXML-MODE-HOOKS."
- (setq-local company-backends '((company-files company-nxml company-dabbrev-code)))
- (flyspell-prog-mode)
-)
-;
-(defun my-shell-mode-hooks()
- "MY-SHELL-MODE-HOOKS."
- (setq-local company-backends '((company-files company-shell company-dabbrev)))
- )
-;
-(defun my-eshell-mode-hooks ()
+(defun my-c++-mode-hooks () 
+  "MY-C++-MODE-HOOKS." 
+  (interactive) 
+  (cmake-ide-setup) 
+  (setq rtags-completions-enabled t) 
+  (setq semantic-mode 1) 
+  (setq-local company-backends '((company-keywords company-rtags))) 
+  (fci-mode) 
+  (flyspell-prog-mode))
+                                        ;
+(defun my-c-mode-hooks () 
+  "MY-C-MODE-HOOKS." 
+  (interactive) 
+  (cmake-ide-setup) 
+  (setq semantic-mode 1) 
+  (setq rtags-completions-enabled t) 
+  (setq-local company-backends '((company-keywords company-rtags))) 
+  (fci-mode) 
+  (flyspell-prog-mode))
+                                        ;
+(defun my-cmake-mode-hooks() 
+  "MY-CMAKE-MODE-HOOKS."
+  (setq-local company-backends '((company-files company-cmake company-dabbrev-code))) 
+  (flyspell-prog-mode))
+                                        ;
+(defun my-org-mode-hooks() 
+  "MY-ORG-MODE-HOOKS."
+  (setq-local company-backends '((company-files company-dabbrev))) 
+  (flyspell-mode 1))
+                                        ;
+(defun my-inferior-octave-mode-hooks() 
+  "MY-Inferior-OCTAVE-MODE-HOOKS."
+  (turn-on-font-lock) 
+  (define-key inferior-octave-mode-map [up] 'comint-previous-input) 
+  (define-key inferior-octave-mode-map [down] 'comint-next-input))
+(defun my-octave-mode-hooks() 
+  "MY-OCTAVE-MODE-HOOKS."
+  (setq-local company-backends '((company-files company-dabbrev-code))) 
+  (fci-mode) 
+  (set-fill-column 80) 
+  (auto-fill-mode 1) 
+  (abbrev-mode 1) 
+  (if (eq window-system 'x) 
+      (font-lock-mode 1))) 
+(flyspell-prog-mode)
+                                        ;
+(defun my-python-mode-hooks() 
+  "MY-PYTHON-MODE-HOOKS."
+  (setq-local company-backends '((company-files company-jedi company-dabbrev-code))) 
+  (require 'sphinx-doc) 
+  (sphinx-doc-mode t) 
+  (require 'py-isort) 
+  (add-hook 'before-save-hook 'py-isort-before-save) 
+  (fci-mode) 
+  (set-fill-column 100) 
+  (flyspell-prog-mode))
+                                        ;
+(defun my-nxml-mode-hooks() 
+  "MY-NXML-MODE-HOOKS."
+  (setq-local company-backends '((company-files company-nxml company-dabbrev-code))) 
+  (flyspell-prog-mode))
+                                        ;
+(defun my-shell-mode-hooks() 
+  "MY-SHELL-MODE-HOOKS."
+  (setq-local company-backends '((company-files company-shell company-dabbrev))))
+                                        ;
+(defun my-eshell-mode-hooks () 
   "My-eshell-mode-hooks."
-  (define-key eshell-mode-map (kbd "C-c C-h")  'helm-eshell-history)
+  (define-key eshell-mode-map (kbd "C-c C-h")  'helm-eshell-history) 
   (define-key minibuffer-local-map (kbd "C-c C-m")  'helm-minibuffer-history) ; list actions using C-z
   (define-key shell-mode-map (kbd "C-c C-r")  'helm-comint-input-ring) ; list actions using C-z
-)
-;
-(defun my-text-mode-hooks()
- "MY-TEXT-MODE-HOOKS."
- (setq-local company-backends '((company-ispell company-files company-dabbrev)))
- (turn-on-auto-fill)
- (fci-mode)
- (set-fill-column 80)
- (flyspell-mode 1)
- )
-;
-(defun my-markdown-mode-hooks()
- "MY-MARKDOWN-MODE-HOOKS."
- (setq-local company-backends '((company-ispell company-files company-dabbrev)))
- (turn-on-auto-fill)
- (fci-mode)
- (set-fill-column 80)
- (flyspell-prog-mode)
-   (when buffer-file-name
-     (add-hook 'after-save-hook 'check-parens nil t))
-; warning, may yield wrong results in edge-cases like single double-quotes in code block.
-; Use only if your files usually are balanced w/r/t double-quotes
-; <http://stackoverflow.com/questions/9527593/>
- (modify-syntax-entry ?\" "\"" markdown-mode-syntax-table)
-)
-;
-(defun my-latex-mode-hook ()
+  )
+                                        ;
+(defun my-text-mode-hooks() 
+  "MY-TEXT-MODE-HOOKS."
+  (setq-local company-backends '((company-ispell company-files company-dabbrev))) 
+  (turn-on-auto-fill) 
+  (fci-mode) 
+  (set-fill-column 80) 
+  (flyspell-mode 1))
+                                        ;
+(defun my-markdown-mode-hooks() 
+  "MY-MARKDOWN-MODE-HOOKS."
+  (setq-local company-backends '((company-ispell company-files company-dabbrev))) 
+  (turn-on-auto-fill) 
+  (fci-mode) 
+  (set-fill-column 80) 
+  (flyspell-prog-mode) 
+  (when buffer-file-name (add-hook 'after-save-hook 'check-parens nil t))
+                                        ; warning, may yield wrong results in edge-cases like single double-quotes in code block.
+                                        ; Use only if your files usually are balanced w/r/t double-quotes
+                                        ; <http://stackoverflow.com/questions/9527593/>
+  (modify-syntax-entry ?\" "\"" markdown-mode-syntax-table))
+                                        ;
+(defun my-latex-mode-hook () 
   "MY-LATEX-MODE-HOOK."
-   (setq-local company-backends '((company-auctex
-                                   company-auctex-bib-candidates
-                                   company-auctex-bibs
-                                   company-math-symbols-latex
-                                   company-math-symbols-unicode
-                                   company-latex-commands
-                                   company-dabbrev-code
-                                   )))
-   (flyspell-mode 1)
-)
-(defun my-lisp-mode-hook ()
+  (setq-local company-backends '((company-auctex company-auctex-bib-candidates company-auctex-bibs
+                                                 company-math-symbols-latex
+                                                 company-math-symbols-unicode company-latex-commands
+                                                 company-dabbrev-code))) 
+  (flyspell-mode 1))
+(defun my-lisp-mode-hook () 
   "MY-elisp-MODE-HOOK."
-  (flyspell-prog-mode)
-)
-(defun my-emacs-lisp-mode-hook ()
+  (flyspell-prog-mode))
+(defun my-emacs-lisp-mode-hook () 
   "MY-emacs-elisp-MODE-HOOK."
-  (setq-local company-backends '((
-  company-dabbrev-code
-  company-files
-  company-oddmuse
-  company-capf
-  )))
-  (focus-mode t)
-  (turn-on-auto-fill)
-  (fci-mode)
-  (set-fill-column 80)
-  (flyspell-prog-mode)
-)
+  (setq-local company-backends '((company-dabbrev-code company-files company-oddmuse company-capf))) 
+  (focus-mode t) 
+  (turn-on-auto-fill) 
+  (fci-mode) 
+  (set-fill-column 80) 
+  (flyspell-prog-mode))
 
 ;;;;
 ;;;; ------ apply-mode-hooks ---------------------------------------------
