b082560ed0c12389c54662ca873bf2489756e169
Oliver Kehret <okehret@gmail.com>
2017-02-11 00:35:52 +0100

    strict-mode

--- emacs/setup/smartparenssetup.el
+++ emacs/setup/smartparenssetup.el
@@ -6,7 +6,8 @@
 (use-package 
   smartparens 
   :ensure t 
-  :init (progn (add-hook 'emacs-lisp-mode-hook #'smartparens-mode) 
+  :init (progn (smartparens-strict-mode) 
+               (add-hook 'emacs-lisp-mode-hook #'smartparens-mode) 
                (add-hook 'js-mode-hook #'smartparens-mode)))
 (provide 'smartparenssetup.el)
 ;;; smartparenssetup.el ends here
