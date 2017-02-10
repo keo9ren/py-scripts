ce437e1ecb32cd4ef2a984ab686be4c253290496
Oliver Kehret <okehret@gmail.com>
2017-02-11 00:40:02 +0100

    formated file

--- emacs/setup/indentguidesetup.el
+++ emacs/setup/indentguidesetup.el
@@ -2,14 +2,12 @@
 ;;; commentary:
 ;;; Code:
 
-(use-package indent-guide
-  :config (progn
-            (indent-guide-global-mode)
-            (set-face-background 'indent-guide-face "dimgray")
-            (setq indent-guide-delay 4)
-            (setq indent-guide-recursive t)
-            (setq indent-guide-char "|")
-            )
-)
+(use-package 
+  indent-guide 
+  :config (progn (indent-guide-global-mode) 
+                 (set-face-background 'indent-guide-face "dimgray") 
+                 (setq indent-guide-delay 4) 
+                 (setq indent-guide-recursive t) 
+                 (setq indent-guide-char "|")))
 (provide 'indentguidesetup.el)
 ;;; indentguidesetup.el ends here
