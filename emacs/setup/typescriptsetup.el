fe63ae69ca55bcec471d88d027b26669b583ecf8
Oliver Kehret <okehret@gmail.com>
2017-02-11 01:05:54 +0100

    formated file

--- emacs/setup/typescriptsetup.el
+++ emacs/setup/typescriptsetup.el
@@ -3,29 +3,28 @@
 ;;; Code:
 
 
-(defun td ()
-  (interactive)
-  (tide-mode 1)
-  (tide-restart-server)
-  (tide-hl-identifier-mode +1)
-  (setq company-tooltip-align-annotations t)
-  (add-hook 'before-save-hook 'tide-format-before-save)
-  (setq tide-tsserver-process-environment '("TSS_LOG=-level verbose -file /tmp/tss.log"))
-  (fci-mode)
-  (set-fill-column 80)
-  (flyspell-prog-mode)
-  (smartparens-mode t)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil))
-  )
+(defun td () 
+  (interactive) 
+  (tide-mode 1) 
+  (tide-restart-server) 
+  (tide-hl-identifier-mode +1) 
+  (setq company-tooltip-align-annotations t) 
+  (add-hook 'before-save-hook 'tide-format-before-save) 
+  (setq tide-tsserver-process-environment '("TSS_LOG=-level verbose -file /tmp/tss.log")) 
+  (fci-mode) 
+  (set-fill-column 80) 
+  (flyspell-prog-mode) 
+  (smartparens-mode t) 
+  (setq flycheck-check-syntax-automatically '(save mode-enabled)) 
+  (setq tide-format-options 
+        '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t 
+                                                                :placeOpenBraceOnNewLineForFunctions
+                                                                nil)))
 
 (require 'use-package)
-
-    (use-package tide
-             :ensure t
-             :init (progn
-                     (with-eval-after-load 'company
-                       (add-to-list 'company-backends 'company-tide))))
-
-        (provide 'typescriptsetup.el)
+(use-package 
+  tide 
+  :ensure t 
+  :init (progn (with-eval-after-load 'company (add-to-list 'company-backends 'company-tide))))
+(provide 'typescriptsetup.el)
 ;;; typescriptsetup.el ends here
