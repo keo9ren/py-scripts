9ae737c337f946214dbd6ffa7cf81b10799eef2f
Oliver Kehret <okehret@gmail.com>
2017-02-11 13:13:31 +0100

    moved hooks to company file

--- emacs/setup/companysetup.el
+++ emacs/setup/companysetup.el
@@ -3,10 +3,8 @@
 ;;; Code:
 
 (require 'package)
-(use-package 
-  company 
-  :init (progn (add-hook 'after-init-hook
-                         'global-company-mode) 
+(use-package company 
+  :init (progn (add-hook 'after-init-hook 'global-company-mode) 
                (setq company-minimum-prefix-length 1) 
                (setq company-idle-delay 0.1) 
                (use-package 
@@ -21,6 +19,37 @@
                                                  (setq-local company-backends
                                                              '((company-dabbrev-code company-files
                                                                                      company-oddmuse
-                                                                                     company-capf)))))))
+                                                                                     company-capf)))))
+               (add-hook 'text-mode-hook (lambda () 
+                                                 (setq-local company-backends
+                                                             '((company-ispell company-files
+                                                                                     company-dabbrev)))))
+               (add-hook 'latex-mode-hook (lambda () 
+                                                 (setq-local company-backends
+                                                             '((company-auctex
+                                                                company-auctex-bib-candidates
+                                                                company-auctex-bibs
+                                                                company-math-symbols-latex
+                                                                company-math-symbols-unicode
+                                                                company-latex-commands
+                                                                                     company-dabbrev)))))
+
+               (add-hook 'cmake-mode-hook (lambda () 
+                                                 (setq-local company-backends
+                                                             '((company-files company-cmake
+                                                                                     company-dabbrev)))))
+               (add-hook 'org-mode-hook (lambda () 
+                                                 (setq-local company-backends
+                                                             '((company-files company-dabbrev)))))
+               (add-hook 'octave-mode-hook (lambda () 
+                                                 (setq-local company-backends
+                                                             '((company-files company-dabbrev)))))
+               (add-hook 'python-mode-hook (lambda () 
+                                                 (setq-local company-backends
+                                                             '((company-files company-jedi company-dabbrev-code)))))
+
+
+                         ))
+
 (provide 'companysetup.el)
 ;;; companysetup.el ends here
--- emacs/setup/mymodehooks.el
+++ emacs/setup/mymodehooks.el
@@ -1,14 +1,6 @@
 ;;; package --- Summary
 ;;; commentary:
 ;;; Code:
-(defun my-cmake-mode-hooks() 
-  "MY-CMAKE-MODE-HOOKS."
-  (setq-local company-backends '((company-files company-cmake company-dabbrev-code))) 
-  )
-(defun my-org-mode-hooks() 
-  "MY-ORG-MODE-HOOKS."
-  (setq-local company-backends '((company-files company-dabbrev))) 
-  )
 (defun my-inferior-octave-mode-hooks() 
   "MY-Inferior-OCTAVE-MODE-HOOKS."
   (turn-on-font-lock) 
@@ -16,13 +8,11 @@
   (define-key inferior-octave-mode-map [down] 'comint-next-input))
 (defun my-octave-mode-hooks() 
   "MY-OCTAVE-MODE-HOOKS."
-  (setq-local company-backends '((company-files company-dabbrev-code))) 
   (abbrev-mode 1) 
   (if (eq window-system 'x) 
       (font-lock-mode 1))) 
 (defun my-python-mode-hooks() 
   "MY-PYTHON-MODE-HOOKS."
-  (setq-local company-backends '((company-files company-jedi company-dabbrev-code))) 
   (require 'sphinx-doc) 
   (sphinx-doc-mode t) 
   (require 'py-isort) 
@@ -41,10 +31,6 @@
   (define-key minibuffer-local-map (kbd "C-c C-m")  'helm-minibuffer-history) ; list actions using C-z
   (define-key shell-mode-map (kbd "C-c C-r")  'helm-comint-input-ring) ; list actions using C-z
   )
-(defun my-text-mode-hooks() 
-  "MY-TEXT-MODE-HOOKS."
-  (setq-local company-backends '((company-ispell company-files company-dabbrev))) 
-  )
 (defun my-markdown-mode-hooks() 
   "MY-MARKDOWN-MODE-HOOKS."
   (setq-local company-backends '((company-ispell company-files company-dabbrev))) 
@@ -52,12 +38,5 @@
                                         ; Use only if your files usually are balanced w/r/t double-quotes
                                         ; <http://stackoverflow.com/questions/9527593/>
   (modify-syntax-entry ?\" "\"" markdown-mode-syntax-table))
-(defun my-latex-mode-hook () 
-  "MY-LATEX-MODE-HOOK."
-  (setq-local company-backends '((company-auctex company-auctex-bib-candidates company-auctex-bibs
-                                                 company-math-symbols-latex
-                                                 company-math-symbols-unicode company-latex-commands
-                                                 company-dabbrev-code))) 
-  )
 (provide 'mymodehooks.el)
 ;;; mymodehooks.el ends here
