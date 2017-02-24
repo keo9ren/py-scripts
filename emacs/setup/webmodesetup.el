d1c82b6f93a49aef2ae9c45ee643360e52672aef
Oliver Kehret <okehret@gmail.com>
2017-02-24 19:12:56 +0100

    dirty but company-tide works with webmode

--- emacs/setup/webmodesetup.el
+++ emacs/setup/webmodesetup.el
@@ -3,6 +3,19 @@
 ;;; Code:
 (require 'use-package)
 
+(defun td () 
+  (interactive) 
+  (tide-mode 1) 
+  (tide-restart-server) 
+  (tide-hl-identifier-mode +1) 
+  (setq company-tooltip-align-annotations t) 
+  (add-hook 'before-save-hook 'tide-format-before-save) 
+  (setq tide-tsserver-process-environment '("TSS_LOG=-level verbose -file /tmp/tss.log")) 
+  (setq flycheck-check-syntax-automatically '(save mode-enabled)) 
+  (setq tide-format-options 
+        '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t 
+                                                                :placeOpenBraceOnNewLineForFunctions
+                                                                nil)))
 (use-package web-mode
   :ensure t
   :mode ("\\.phtml\\'"
@@ -13,12 +26,23 @@
          "\\.djhtml\\'"
          "\\.erb\\'"
          "\\.mustache\\'"
-         ;"\\.ts\\'"
+         "\\.ts\\'"
          "\\.tsx\\'"
          "\\.js\\'"
          "\\.jsx\\'"
          "\\.json\\'"
          )
+  :init (progn
+          (add-hook 'web-mode-hook '(lambda ()
+                                                 (tide-mode)
+                                                 (tide-hl-identifier-mode +1)
+                                                 (setq-local company-backends '((company-keywords company-tide)))
+                                                 (setq-local company-tooltip-align-annotations)
+                                                 (add-hook 'before-save-hook 'tide-format-before-save) 
+                                                 (setq tide-tsserver-process-environment '("TSS_LOG=-level verbose -file /tmp/tss.log")) 
+                                                 (setq flycheck-check-syntax-automatically '(save mode-enabled)) 
+                                      ))
+           )
 )
 (provide 'webmodesetup.el)
 ;;; webmodesetup.el ends here
