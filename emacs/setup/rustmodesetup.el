3bf7ac26c1210e5e6a97f9ec278e26c2b46f7631
Oliver Kehret <okehret@gmail.com>
2017-08-26 22:25:51 +0200

    make rustfmt work

--- emacs/setup/rustmodesetup.el
+++ emacs/setup/rustmodesetup.el
@@ -3,6 +3,10 @@
 ;;; Code:
 (require 'package)
 (use-package rust-mode
+  :init(progn
+        (setq rust-rustfmt-bin "rustfmt")
+        (setq rust-format-on-save t)
+        )
 )
 (provide 'rustmodesetup.el)
 ;;; rustmodesetup.el ends here
